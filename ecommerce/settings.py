f6f5e731faf6819e6d279f55792ef7f31b0d80a3
Mark Dyson <mark.dyson1@outlook.com>
2020-05-23 16:14:56 +0000

    m

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1', '8000-c5489b6f-391a-49c2-a0d0-cd1d25162843.ws-eu01.gitpod.io']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1', '8000-c5489b6f-391a-49c2-a0d0-cd1d25162843.ws-eu01.gitpod.io', 'silverheath-phones.herokuapp.com']
 
 
 # Application definition
