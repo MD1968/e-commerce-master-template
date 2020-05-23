c328d6cebf8725a6c90e932f75a79ddeecf290e0
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-05-23 19:54:26 +0100

    wdw

--- search/views.py
+++ search/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from products.models import Product
 
 # Create your views here.
-def do_search(request):
-    products = products.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+
+
+def search(request):
+    """ Display search results """
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "search.html", {"products": products})
