51f44ec8a510738f7017446601389b28424644c1
Mark Dyson <mark.dyson1@outlook.com>
2020-05-24 08:48:50 +0000

    test

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import do_search
 
 urlpatterns = [
-    path('', do_search, name='search'),
-    ]
+    url(r'^$', do_search, name='search')
+]
--- search/views.py
+++ search/views.py
@@ -1,36 +1,7 @@
-from django.shortcuts import render, redirect, reverse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib import messages
-from django.db.models import Q
+from django.shortcuts import render
 from products.models import Product
 
-
+# Create your views here.
 def do_search(request):
-    product_list = Product.objects.all()
-    query = request.GET.get('q')
-    if query:
-        product_list = Product.objects.filter(
-            Q(name__icontains=query) | Q(description__icontains=query)
-        ).distinct()
-    paginator = Paginator(product_list, 6)
-    page = request.GET.get('page')
-
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        products = paginator.page(1)
-    except EmptyPage:
-        products = paginator.page(paginator.num_pages)
-
-    context = {
-        'products': products
-    }
-
-    amount = len(product_list)
-    if amount == 0:
-        messages.info(request, 'Unfortunately, your search returned no items.')
-        return redirect(reverse('index'))
-    elif amount > 0:
-        return render(request, "products.html", context)
-
-
+    products = Products.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products.html", {"products": products})
