eb233fe7867c1a067a11f84601e4faf1514346ba
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-05-24 09:28:39 +0100

    wqeqwe

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.conf.urls import url
 from .views import do_search
 
 urlpatterns = [
-    path('', do_search, name='search'),
-    ]
+    url(r'^$', do_search, name='search')
+]
